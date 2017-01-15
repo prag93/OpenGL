8c56be0ddbaf8ecd9fe73ff5583baca3225c2298
prag93 <prashantgupta@Prashants-MacBook-Air-2.local>
2017-01-14 21:50:08 -0800

    Creating Blank Window and using ESC key to exit

---
+++ OpenGL/Creating Blank Window.cpp
@@ -0,0 +1,61 @@
+
+
+// GLEW
+#define GLEW_STATIC
+#include "glew.h"
+// GLFW
+#include "glfw3.h"
+#include <iostream>
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+    // When a user presses the escape key, we set the WindowShouldClose property to true,
+    // closing the application
+    if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+    
+    if (window==nullptr){
+        std::cout<< "Failed to create window"<< std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK)
+    {
+        std::cout << "Failed to initialize GLEW" << std::endl;
+        return -1;
+    }
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    
+    glViewport(0, 0, width, height);
+    
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+        glfwSetKeyCallback(window, key_callback);
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+    }
+    
+    
+    
+    glfwTerminate();
+    
+    return 0;
+}
